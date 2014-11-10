b457c55002524283ffefe2f11a1c75ea05feb5b6
Tr1ckX <attila.mirk@gmail.com>
2014-11-10 13:15:46 +0000

    Pill has finished

--- hello.rb
+++ hello.rb
@@ -9,7 +9,7 @@ require 'sinatra'
   # end
 
   get '/' do
-    "<div style='border: 3px dashed red'>
-     <img src='http://bit.ly/1eze8aE'>
-   </div>"
+    @visitor = params[:name]
+    @name = %w(Amigo Oscar Viking).sample
+    erb :index
   end
